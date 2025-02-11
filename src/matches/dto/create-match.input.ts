b32d9ce1e264baa46522568fcdc5d78fc2609722
Anguzu Daniel <anguzud7@gmail.com>
2025-02-11 21:48:06 +0300

    add createMatchInput

--- src/matches/dto/create-match.input.ts
+++ src/matches/dto/create-match.input.ts
@@ -1,7 +1,31 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { MatchStatus } from '../../enums/MatchStatus';
+import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
 export class CreateMatchInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @ApiProperty()
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Field()
+  @Column()
+  userId: string;
+
+  @ApiProperty()
+  @Field()
+  @Column()
+  matchedUserId: string;
+
+  @ApiProperty()
+  @Field()
+  @Column({
+    type: 'enum',
+    enum: MatchStatus,
+    default: MatchStatus.PENDING,
+  })
+  static: MatchStatus;
 }
