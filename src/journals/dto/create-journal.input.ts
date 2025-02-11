c6cfbdc8b60fcfaea63ac44940bcca7331c00b0e
Anguzu Daniel <anguzud7@gmail.com>
2025-02-11 16:52:01 +0300

    add journal entity

--- src/journals/dto/create-journal.input.ts
+++ src/journals/dto/create-journal.input.ts
@@ -1,7 +1,33 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Users } from '../../entities/users.entity';
 
 @InputType()
 export class CreateJournalInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  @Column()
+  id: number;
+
+  @Field(() => String)
+  @Column()
+  @ManyToOne(() => Users, { onDelete: 'CASCADE' })
+  userId: string;
+
+  @Field()
+  @Column('text')
+  content: string;
+
+  @Field()
+  @Column('decimal', { precision: 5, scale: 2 })
+  sentimentScore: string;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
 }
--- src/journals/entities/journal.entity.ts
+++ src/journals/entities/journal.entity.ts
@@ -1,7 +1,33 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Users } from '../../entities/users.entity';
 
 @ObjectType()
 export class Journal {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  @Column()
+  id: number;
+
+  @Field(() => String)
+  @Column()
+  @ManyToOne(() => Users, { onDelete: 'CASCADE' })
+  userId: string;
+
+  @Field()
+  @Column('text')
+  content: string;
+
+  @Field()
+  @Column('decimal', { precision: 5, scale: 2 })
+  sentimentScore: string;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
 }
