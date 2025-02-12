93fb4592f2d439bde8b41fb711392a2aed3cb432
Anguzu Daniel <anguzud7@gmail.com>
2025-02-12 11:28:44 +0300

    add resource entity

--- src/resources/entities/resource.entity.ts
+++ src/resources/entities/resource.entity.ts
@@ -1,15 +1,31 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { ApiProperty } from '@nestjs/swagger';
+import { ResourceType } from '../../enums/ResourceType';
+import { Column } from 'typeorm';
 
 @ObjectType()
 export class Resource {
   @ApiProperty()
   @Field(() => Int)
+  @Column()
   id: number;
 
   @ApiProperty()
   @Field()
+  @Column()
   title: string;
 
+  @ApiProperty()
+  @Field()
+  @Column({
+    type: 'enum',
+    enum: ResourceType,
+    default: ResourceType.ARTICLE,
+  })
+  type: ResourceType;
 
+  @ApiProperty()
+  @Field()
+  @Column()
+  url: string;
 }
