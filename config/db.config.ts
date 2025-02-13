3abe16208ac87789cd65bdfed222d54b1604c6dd
Anguzu Daniel <anguzud7@gmail.com>
2025-02-13 13:30:56 +0300

    update config

--- config/db.config.ts
+++ config/db.config.ts
@@ -1,6 +1,10 @@
-import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions.js';
 import { registerAs } from '@nestjs/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default registerAs(
   'dbconfig.dev',
@@ -8,7 +12,7 @@ export default registerAs(
     url: process.env.POSTGRES_URL,
     type: 'postgres',
 
-    entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
 
     synchronize: true,
   }),
